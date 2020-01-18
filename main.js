51b04b8ab2e526628197acecc6113b3f1d60bb05
capillaryking <49357636+capillaryking@users.noreply.github.com>
2020-01-17 19:55:13 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,244 @@
+let body = document.body
+
+let setupTestDiv = document.getElementById("setupTestDiv")
+let uploadFileInput = document.getElementById("uploadFile")
+let textPasteInput = document.getElementById("pasteFile")
+
+let startTestButton = document.getElementById("start-test")
+let canStartTest = false
+
+
+function shuffle(array) { // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+
+function makeAlert(type, text) {
+    let alert = document.createElement("div")
+    alert.className = "alert alert-" + type
+    alert.role = "alert"
+    alert.textContent = text
+    return alert
+}
+
+function generateQuestionDiv(question, questionNumber, totalQuestions) {
+    let div = document.createElement("div")
+    let heading = document.createElement("h1")
+    heading.textContent = "Question " + (questionNumber + 1) + " of " + totalQuestions
+    
+    let ask = document.createElement("p")
+    ask.textContent = question.ask
+
+    let object = {div: div}
+    // call check on object to check it
+    
+    div.appendChild(heading)
+    div.appendChild(ask)
+    if (question.type == "multiple-choice") {
+        let selected
+        object.check = function() {
+            let correct = (selected === question.correct)
+            for (child of div.childNodes) {
+                if (child.className == "form-check") {
+                    for (child1 of child.childNodes) {
+                        child1.disabled = true
+                    }
+                }
+            }
+
+            if (!correct) {
+                div.insertBefore(makeAlert("danger", "You got it wrong! The correct answer was '" + question.choices[question.correct] + "' you chose '" + question.choices[selected] + ".'"), ask)
+            } else {
+                div.insertBefore(makeAlert("success", "You got it correct!"), ask) 
+            }
+            
+            return correct
+        }
+
+        for (let i=0;i<question.choices.length;i++) {
+            let choice = question.choices[i]
+            let formCheck = document.createElement("div")
+            formCheck.className = "form-check"
+
+            let input = document.createElement("input")
+            input.className = "form-check-input"
+            input.name = "choiceRadios" + questionNumber
+            input.id = "thisIsMyChoice" + choice + questionNumber
+            input.type = "radio"
+            
+            let label = document.createElement("label")
+            label.className = "form-check-label"
+            label.for = input.id
+            label.textContent = choice
+
+            input.addEventListener("click", () => {
+                selected = i
+            })
+            formCheck.appendChild(input)
+            formCheck.appendChild(label)
+            div.appendChild(formCheck)
+        }
+    } else if (question.type == "type-out") {
+        object.check = function() {
+            let correct = true
+            let mappedCorrect = question.correct.map(x => x.toLowerCase())
+            let notMissing = []
+
+            for (child of div.childNodes) {
+                if (child.className == "form-control") {
+                    let included = mappedCorrect.includes(child.value.toLowerCase())
+                    if (!included) {
+                        correct = false
+                    } else {
+                        notMissing[child.value.toLowerCase()] = true
+                    }
+                    child.disabled = true
+                }
+            }
+
+            
+
+            if (!correct) {
+                let missing = question.correct.filter(x => (!notMissing[x])).map(x => "'" + x + "'")
+                let mapped = question.correct.map(x => "'" + x + "'")
+                
+                div.insertBefore(makeAlert("danger", "You got it wrong! The corrects answer were '" + mapped.join(", ") + ". You were missing: " + missing.join(", ") + "."), ask)
+            } else {
+                div.insertBefore(makeAlert("success", "You got it correct!"), ask) 
+            }
+            
+            return correct
+        }
+
+        for (let i=0;i<question.inputs;i++) {
+            let input = document.createElement("input")
+            input.type = "text"
+            input.className = "form-control"
+            input.style.marginBottom = "3px"
+            
+            div.appendChild(input)
+        }
+    }
+
+    return object
+}
+
+function checkIfValidTest(test) {
+    if (!test.questions) return
+    if (!test.title) return
+
+    return true
+}
+
+function startTest() {
+    let test = canStartTest
+    if (!test) return
+    if (!checkIfValidTest(test)) return
+
+    if (test.random) {
+        test.questions = shuffle(test.questions)
+    }
+
+    let questionObjects = []
+    let currentQuestion = 0
+    
+    for (let i=0;i<test.questions.length;i++) {
+        questionObjects[i] = generateQuestionDiv(test.questions[i], i, test.questions.length)
+    }
+
+    setupTestDiv.style.display = "none"
+    
+    let nextQuestion = document.createElement("button")
+    nextQuestion.className = "btn btn-primary"
+    nextQuestion.style.marginLeft = "5px"
+    nextQuestion.textContent = "next"
+
+    let prevQuestion = document.createElement("button")
+    prevQuestion.className = "btn btn-secondary"
+    prevQuestion.textContent = "previous"
+
+    function addDiv(div) {
+        body.insertBefore(div, prevQuestion)
+    }
+
+    nextQuestion.addEventListener("click", () => {
+        if (currentQuestion >= (test.questions.length-1)) { // test finished
+            body.removeChild(questionObjects[currentQuestion].div)
+            nextQuestion.style.display = "none"
+            prevQuestion.style.display = "none"
+
+            for (object of questionObjects) {
+                object.check()
+                addDiv(object.div)
+            }
+        } else {
+            body.removeChild(questionObjects[currentQuestion].div)
+            currentQuestion += 1
+            addDiv(questionObjects[currentQuestion].div)
+            if (currentQuestion >= (test.questions.length-1)) {
+                nextQuestion.className = "btn btn-success"
+                nextQuestion.textContent = "finish"
+            }
+        }
+    })
+
+    prevQuestion.addEventListener("click", () => {
+        if (currentQuestion > 0) {
+            body.removeChild(questionObjects[currentQuestion].div)
+            currentQuestion -= 1
+            addDiv(questionObjects[currentQuestion].div)
+            nextQuestion.className = "btn btn-primary"
+            nextQuestion.textContent = "next"
+        }
+    })
+
+    body.appendChild(prevQuestion)
+    body.appendChild(nextQuestion)
+    addDiv(questionObjects[currentQuestion].div)  
+}
+
+function checkIfCanStart() {
+    let value = textPasteInput.value
+    let couldStart = canStartTest
+    let canParse = true
+
+    try {
+        canStartTest = JSON.parse(value)
+    } catch {
+        canParse = false
+        canStartTest = false
+    }
+
+    if (canParse && canStartTest) {
+        startTestButton.removeAttribute("disabled")
+    } else if (couldStart) {
+        startTestButton.setAttribute("disabled", true)
+    }
+}
+
+uploadFileInput.addEventListener("change", async () => {
+    let file = uploadFileInput.files.item(0)
+    if ((file.type == "application/json") || (file.type == "text/plain")) {
+        let text = await file.text()
+        textPasteInput.value = text
+        checkIfCanStart()
+    }
+})
+
+startTestButton.addEventListener("click", startTest)
+textPasteInput.addEventListener("change", checkIfCanStart)
+checkIfCanStart()
